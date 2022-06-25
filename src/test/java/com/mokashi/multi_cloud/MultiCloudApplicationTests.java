4661927c8b8b5c57e22423f4e92923e19d038f57
sanket <sanketm221995@gmail.com>
2022-06-25 09:33:51 +0530

    Application Initialized

---
+++ src/test/java/com/mokashi/multi_cloud/MultiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokashi.multi_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
